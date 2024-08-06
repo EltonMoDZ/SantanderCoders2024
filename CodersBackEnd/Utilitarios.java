829564a939d18704d05860bf9e42203b8eb642d0
Elton Alcantara Costa <eltonalcancosta@gmail.com>
2024-08-05 21:17:03 -0300

    feat: Adicionar classe Utilitarios para conversões e cálculos
    A classe Utilitarios foi adicionada para fornecer métodos de conversão de temperatura e cálculos relacionados a círculos. Essa classe inclui métodos para converter de Celsius para Fahrenheit e vice-versa, calcular o diâmetro de um círculo e calcular a área de um círculo. Além disso, a classe também inclui uma constante PI para uso nos cálculos.
    
    Co-authored-by: [author-name]

---
+++ CodersBackEnd/Utilitarios.java
@@ -0,0 +1,35 @@
+public class Utilitarios {
+    public static final Double PI = 3.14;
+
+    public static Double conversorCelciusparaFahrenheit(Double x){
+        return (x * 1.8) + 32;
+    }
+
+    public static Double conversorFahrenheitparaCelcius(Double x){
+        return (x - 32) * 5 / 9;
+    }
+
+    public static String calculoDeDiametro(Double raio){
+        if (raio < 0) {
+            throw new IllegalArgumentException("O raio deve ser positivo.");
+        }
+        final Double VALOR = 2 * PI * raio;
+        final String RESULTADO = String.format("%.2f", VALOR);
+        return RESULTADO;
+    }
+
+    public static double calcularAreaCirculo(double raio) {
+        if (raio < 0) {
+            throw new IllegalArgumentException("O raio deve ser positivo.");
+        }
+        return PI * raio * raio;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(conversorCelciusparaFahrenheit(32.0));
+        System.out.println(calculoDeDiametro(20.0));
+        System.out.println(conversorFahrenheitparaCelcius(32.0));
+        System.out.println(calcularAreaCirculo(40));
+    }
+}
